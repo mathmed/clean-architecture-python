5b1557f02c373edb174c869264945842d92a04ba
Mateus Medeiros <mateusmedeiros252525@hotmail.com>
2021-07-09 14:55:20 -0300

    feat: add alchemy config

---
+++ src/infra/database/config/db_config.py
@@ -0,0 +1,31 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+load_dotenv()
+
+class DBConnectionHandler:
+    """ SQL Alchemy Database Connection """
+
+    def __init__(self):
+        self.__connection_string = "mysql+pymysql://{}:{}@{}:{}/{}?charset=utf8mb4".format(os.getenv("DB_USER"), os.getenv("DB_PASSWORD"), os.getenv("DB_HOST"), os.getenv("DB_PORT"), os.getenv("DATABASE_NAME"))
+        self.session = None
+
+    def get_engine(self):
+        """
+            Return connection engine
+            :param: None
+            :return: engine connection to Database
+        """
+        engine = create_engine(self.__connection_string)
+        return engine
+
+    def __enter__(self):
+        engine = create_engine(self.__connection_string)
+        session_maker = sessionmaker()
+        self.session = session_maker(bind=engine)
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.session.close() # pylint: disable=no-member
