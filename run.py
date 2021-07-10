96f434995242975d4b78e156455adb70ead3fee8
Mateus Medeiros <mateusmedeiros252525@hotmail.com>
2021-07-10 12:00:42 -0300

    refactor: add dynamic port

--- run.py
+++ run.py
@@ -1,4 +1,8 @@
+import os
 from src.main.config import app
+from dotenv import load_dotenv
+
+load_dotenv()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=os.getenv("APP_PORT"))
