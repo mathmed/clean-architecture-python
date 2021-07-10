56047087c233f5892fa7aac5efd327f8c01e2e07
Mateus Medeiros <mateusmedeiros252525@hotmail.com>
2021-07-09 21:16:44 -0300

    test: ensure user repository create and select user correctly

---
+++ src/infra/database/repositories/user_repository_test.py
@@ -0,0 +1,35 @@
+from faker import Faker
+from src.infra.database.config import DBConnectionHandler
+from src.data.contracts import CreateUserParams
+from .user_repository import UserRepository
+
+
+faker = Faker()
+user_repository = UserRepository()
+db_connection_handler = DBConnectionHandler()
+
+def test_insert_and_select_user():
+    """Should insert and select correctly user"""
+    username= faker.name()
+    name=faker.name()
+    last_name=faker.name()
+    profile_image_url=faker.word()
+    bio=faker.word()
+    email=faker.email()
+    gender="male"
+
+    engine = db_connection_handler.get_engine()
+
+    user = user_repository.insert_user(CreateUserParams(username, name, last_name, profile_image_url, bio, email, gender))
+    select_user = user_repository.select_user("username", username)
+
+    assert user.username == select_user.username
+    assert user.name == select_user.name
+    assert user.last_name == select_user.last_name
+    assert user.profile_image_url == select_user.profile_image_url
+    assert user.bio == select_user.bio
+    assert user.email == select_user.email
+    assert user.gender == select_user.gender
+
+    engine.execute("DELETE FROM users WHERE username = '{}';".format(user.username))
+
