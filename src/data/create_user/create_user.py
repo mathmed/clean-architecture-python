185903aaf1a96bb5f13bc523303e6481670673ec
Mateus Medeiros <mateusmedeiros252525@hotmail.com>
2021-07-09 21:17:05 -0300

    feat: implement create user logic

---
+++ src/data/create_user/create_user.py
@@ -0,0 +1,18 @@
+from typing import Type, Dict
+from src.domain.usecases import CreateUserParams, CreateUser as CreateUserContract
+from src.data.contracts import UserRepositoryContract, CreateUserParams as CreateUserDBParams
+from src.domain.models import User
+
+class CreateUser(CreateUserContract):
+
+    def __init__(self, user_repository: Type[UserRepositoryContract]):
+        self.user_repository = user_repository
+
+    def execute(self, params: CreateUserParams ) -> Dict:
+        
+        response = self.user_repository.insert_user(
+            CreateUserDBParams(
+                params.username, params.name, params.email, params.last_name, params.profile_image_url, params.bio, params.gender
+                )
+            )
+        return {"success": True, "message": "ok", "data": response}
