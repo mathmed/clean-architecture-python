3275447c8b3f3d5e6ec85287158b57cac82ce60f
Mateus Medeiros <mateusmedeiros252525@hotmail.com>
2021-07-10 11:18:44 -0300

    feat: implement adapter to flask

---
+++ src/main/adapter/__init__.py
@@ -0,0 +1,1 @@
+from .route_adapter import flask_adapter
---
+++ src/main/adapter/route_adapter.py
@@ -0,0 +1,34 @@
+from typing import Type
+from sqlalchemy.exc import IntegrityError
+from src.presentation.helpers import HttpRequest, HttpResponse
+from src.presentation.errors import HttpErrors
+from src.presentation.contracts import ControllerContract
+
+def flask_adapter(request: any, api_route: Type[ControllerContract]) -> any:
+
+    try:
+        query_string_params = request.args.to_dict()
+    except:
+        https_error = HttpErrors.error_400()
+        return HttpResponse(
+            status_code=https_error["status_code"], body=https_error["body"]
+        )
+
+    http_request = HttpRequest(
+        header=request.headers, body=request.json, query=query_string_params
+    )
+
+    try:
+        response = api_route.handle(http_request)
+
+    except IntegrityError:
+        https_error = HttpErrors.error_409()
+        return HttpResponse(
+            status_code=https_error["status_code"], body=https_error["body"]
+        )
+    except:
+        https_error = HttpErrors.error_500()
+        return HttpResponse(
+            status_code=https_error["status_code"], body=https_error["body"]
+        )
+    return response
