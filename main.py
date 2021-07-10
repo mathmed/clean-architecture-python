2953ea99137eb3728512ce54d86e502daf607cd8
Mateus Medeiros <mateusmedeiros252525@hotmail.com>
2021-07-10 12:43:21 -0300

    refactor: change run to main

--- main.py
+++ main.py
@@ -4,5 +4,4 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=os.getenv("APP_PORT"))
+app.run(host="0.0.0.0", port=os.getenv("APP_PORT"))
