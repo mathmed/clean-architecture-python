0571716ed29e5a3657c397b9affad5c5029c9a44
Mateus Medeiros <mateusmedeiros252525@hotmail.com>
2021-07-12 18:28:25 -0300

    feat: add create user from github route

--- src/main/routes/api_routes.py
+++ src/main/routes/api_routes.py
@@ -1,6 +1,6 @@
 from flask import jsonify, Blueprint, request
 from src.main.adapter import flask_adapter
-from src.main.factories import create_user_factory
+from src.main.factories import create_user_factory, create_user_from_github_factory
 
 api_routes = Blueprint("api_routes", __name__)
 
@@ -8,10 +8,27 @@ api_routes = Blueprint("api_routes", __name__)
 def create_user():
 
     response = flask_adapter(request=request, api_route=create_user_factory())
-    
     if response.status_code < 300:
         return jsonify({"data": response.body}), response.status_code
+    # Handling Errors
+    return (
+        jsonify(
+            {
+                "error": {
+                    "status": response.status_code,
+                    "title": response.body["error"],
+                }
+            }
+        ),
+        response.status_code,
+    )
 
+@api_routes.route("/api/user/github", methods=["POST"])
+def create_user_from_github():
+
+    response = flask_adapter(request=request, api_route=create_user_from_github_factory())
+    if response.status_code < 300:
+        return jsonify({"data": response.body}), response.status_code
     # Handling Errors
     return (
         jsonify(
