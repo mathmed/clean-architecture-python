b5897089a40819a6c4c5ddf6218f7f2aa652e127
Mateus Medeiros <mateusmedeiros252525@hotmail.com>
2021-07-15 20:12:51 -0300

    feat: add update user route

--- src/main/routes/api_routes.py
+++ src/main/routes/api_routes.py
@@ -1,6 +1,6 @@
 from flask import jsonify, Blueprint, request
 from src.main.adapter import flask_adapter
-from src.main.factories import create_user_factory, create_user_from_github_factory
+from src.main.factories import create_user_factory, create_user_from_github_factory, update_user_factory
 
 api_routes = Blueprint("api_routes", __name__)
 
@@ -27,6 +27,26 @@ def create_user():
 def create_user_from_github():
 
     response = flask_adapter(request=request, api_route=create_user_from_github_factory())
+    if response.status_code < 300:
+        return jsonify({"data": response.body}), response.status_code
+    # Handling Errors
+    return (
+        jsonify(
+            {
+                "error": {
+                    "status": response.status_code,
+                    "title": response.body["error"],
+                }
+            }
+        ),
+        response.status_code,
+    )
+
+
+@api_routes.route("/api/user/<username>", methods=["PATCH"])
+def update_user(username):
+
+    response = flask_adapter(request=request, api_route=update_user_factory(), url_param=username)
     if response.status_code < 300:
         return jsonify({"data": response.body}), response.status_code
     # Handling Errors
